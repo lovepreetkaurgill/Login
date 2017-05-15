319c2405be9908323e43404ed0b13848a299be56
LovepreetKaur <LovepreetKaur>
2017-05-15 15:46:47 +0530

    Initial commit

---
+++ app/src/main/java/com/auribises/login/Guard_loginActivity.java
@@ -0,0 +1,20 @@
+package com.auribises.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class Guard_loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guard_login);
+    }
+    public void registerClickHandler(View view){
+        Intent i = new Intent(Guard_loginActivity.this,Guard_loginActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
